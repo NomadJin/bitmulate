6a99f114d175d8cdf2df503839ce21a311fe0b19
NomadJin <jinnykim000@gmail.com>
2018-10-08 20:59:21 +0900

    routing and page template

--- bitmulate-frontend/src/components/App.js
+++ bitmulate-frontend/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import { Button } from './index'
+import { Route } from 'react-router-dom'
+import { HomePage, TradePage } from './index'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Button/>
+               <Route exact path="/" component={HomePage}/>
+               <Route path="/trade" component={TradePage}/>
             </div>
         )
     }
