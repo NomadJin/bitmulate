a4eb67d6c57fbbfe110835f6009ffc76f7b60176
NomadJin <jinnykim000@gmail.com>
2018-10-20 18:54:24 +0900

    Initialized ScreenMask commit

--- bitmulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
+++ bitmulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './ScreenMask.scss'
 import classNames from 'classnames/bind'
 
-const cx = classNames.bind()
+const cx = classNames.bind(styles)
 
-const ScreenMask = () => {
+const ScreenMask = ({visible}) => {
     return (
         <div className={cx('screen-mask')}>
 
