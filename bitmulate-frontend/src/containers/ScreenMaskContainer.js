62210ae2fc8a86488879bf92190a5ef1cf56a27f
NomadJin <jinnykim000@gmail.com>
2018-10-23 23:06:58 +0900

    hide scroll bar

--- bitmulate-frontend/src/containers/ScreenMaskContainer.js
+++ bitmulate-frontend/src/containers/ScreenMaskContainer.js
@@ -1,8 +1,24 @@
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { ScreenMask } from '../components'
+
+
+class ScreenMaskContainer extends Component {
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.visible !== this.props.visible) {
+            document.body.style.overflowY = this.props.visible ? 'hidden' : 'auto'
+        }
+    }
+
+    render() {
+        return (<ScreenMask {...this.props}/>)
+    }
+
+}
 
 export default connect(
     (state) => ({
         visible: state.base.getIn(['screenMask', 'visible'])
     })
-)(ScreenMask)
+)(ScreenMaskContainer)
