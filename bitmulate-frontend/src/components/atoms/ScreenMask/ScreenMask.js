06fbd97cc1e83b2be4009a16213d8ce93dc43489
NomadJin <jinnykim000@gmail.com>
2018-10-25 19:12:05 +0900

    ScreenMask react-transition

--- bitmulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
+++ bitmulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import styles from './ScreenMask.scss'
 import classNames from 'classnames/bind'
+import CSSTransition from 'react-transition-group/CSSTransition'
+
+const Fade = ({ children, ...props }) => (
+    <CSSTransition
+      {...props}
+      timeout={500}
+      classNames={cx('fade')}  
+    >
+      {children}
+    </CSSTransition>
+)
 
 const cx = classNames.bind(styles)
 
@@ -8,11 +19,9 @@ const ScreenMask = ({visible}) => {
     if(!visible) return null
 
     return (
-        <div className={cx('wrapper')}>
-            <div className={cx('screen-mask')}>
-
-            </div>
-        </div>
+        <Fade in={false}>
+            { visible && <div className={cx('screen-mask')}/> }
+        </Fade>
     )
 }
 
