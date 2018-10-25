31d03ea66a65557ec1dae1ab08e69b86f979b939
NomadJin <jinnykim000@gmail.com>
2018-10-25 23:11:03 +0900

    fade in/out setting

--- bitmulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
+++ bitmulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -6,12 +6,12 @@ import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 
 const ScreenMask = ({ visible }) => (
-  <Transition in={visible} timeout={300}>
+  <Transition in={visible} timeout={150}>
     {(state) => {
         console.log(state)
         if(state === 'exited') return null
         return (
-        <div className={cx(`fade fade-${state}`)}>
+        <div className={cx(`fade-${state}`)}>
           <div className={cx('screen-mask')}/>
         </div>)
     }}
